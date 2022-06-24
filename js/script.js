4e1ca8f58d32ab635096ff857d865123f5c0994c
Gabridg <gabri99.del@gmail.com>
2022-06-24 09:46:00 +0200

    cmplte

--- js/script.js
+++ js/script.js
@@ -30,12 +30,12 @@ Se sei a corto di idee per lo stile, potresti prendere spunto dallo screenshot f
 
 // 1-
 for (let i = 1; i <= 100; i++) {
-    if (i % 3 == 0) {
-        console.log('FIZZ');
+    if (i % 5 == 0 && i % 3 == 0) {
+        console.log('FIZZBUZZ');
     } else if (i % 5 == 0) {
         console.log('BUZZ');
     } else if (i % 5 == 0 && i % 3 == 0) {
-        console.log('FIZZBUZZ')
+        console.log('FIZZ')
     } else {
         console.log(i);
     }
